0dfb8c908dc068c3c6db2a230e745422465bb2f6
Kenan Kocadurdu <kocadurdu@gmail.com>
2026-03-13 13:18:03 +0300

    feat: add WAL-G S3 archiving with custom postgres image (step 0.17)

---
+++ infra/postgres/walg-env.sh
@@ -0,0 +1,5 @@
+export WALG_S3_PREFIX=s3://psikoport-backups/wal
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+export AWS_ENDPOINT=http://minio:9000
+export AWS_S3_FORCE_PATH_STYLE=true
